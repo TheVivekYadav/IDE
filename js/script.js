7ec5b5f3406452f8f31fd4a3a1870f17c08b91f4
vivek yadav <vivekyadavclasher@gmail.com>
2022-08-03 11:38:27 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function output(){
+    var text_area_html;
+    var text_are_css;
+    text_area_html = document.getElementById("html").value;
+    text_are_css = document.getElementById("css").value;
+    console.log(text_area_html,text_are_css)
+
+}
